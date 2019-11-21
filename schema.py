e8c94b1ee06acc82032db9480fbbce7975b5e594
Ruth Brand <ruthbrandsoprano@gmail.com>
2019-11-20 18:49:43 -0800

    fixes string literal error

--- schema.py
+++ schema.py
@@ -10,7 +10,7 @@ cur_object = connection.cursor()
 # i'm trying to see if i should auto-increment the times this button was pushed here
 # or in my actions section. ...i think it will be better in actions but i will try it here
 # first
-def createNegative():
-    table = ("CREATE TABLE negative;
+def createTable(name):
+    table = """CREATE TABLE {}(
             (primary_key INT(11) AUTO_INCREMENT;
-            times_pressed INT(11) AUTO_INCREMENT")
+            times_pressed INT(11) AUTO_INCREMENT)""".format(name)
